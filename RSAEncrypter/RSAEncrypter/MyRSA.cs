809546baa4e97feb53856ba5254bc8f7b903f6eb
Sparrow1488 <ilyaokubev@gmail.com>
2021-03-26 21:55:55 +0300

    create[MyRSA]: create first library

---
+++ RSAEncrypter/RSAEncrypter/MyRSA.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+
+namespace RSAEncrypter
+{
+    public static class MyRSA
+    {
+        public static byte[] EncryptData(byte[] data, RSAParameters publicKey)
+        {
+            var csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(publicKey);
+            var _encryptData = csp.Encrypt(data, false);
+            return _encryptData;
+        }
+        public static byte[] DecryptData(byte[] data, RSAParameters privateKey)
+        {
+            var csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(privateKey);
+            var _decryptData = csp.Decrypt(data, false);
+            return _decryptData;
+        }
+        public static string RsaToString(RSAParameters key)
+        {
+            var sw = new StringWriter();
+            var xmlSerializer = new XmlSerializer(typeof(RSAParameters));
+            xmlSerializer.Serialize(sw, key);
+            string stringKey = sw.ToString();
+            return stringKey;
+        }
+        public static RSAParameters StringToRsa(string key)
+        {
+            var sr = new StringReader(key);
+            var xmlDeserializer = new XmlSerializer(typeof(RSAParameters));
+            var rsaKey = (RSAParameters)xmlDeserializer.Deserialize(sr);
+            return rsaKey;
+        }
+    }
+}
